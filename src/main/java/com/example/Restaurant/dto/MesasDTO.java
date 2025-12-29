d7f92388b49df1dfceb14e9e6f3dba0a7912436b
Kenneth Luera <kennethpl30.5@gmail.com>
2025-12-29 18:24:41 -0500

    correccion

--- src/main/java/com/example/Restaurant/dto/MesasDTO.java
+++ src/main/java/com/example/Restaurant/dto/MesasDTO.java
@@ -8,8 +8,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
